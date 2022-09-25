18e66e6a0d95a33c081015037b7c26f592ce0f90
Shantanu <sborkar9545@gmail.com>
2022-09-25 17:51:57 +0530

    hqlo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
-import routes from './config/Routes';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import routes from "./config/Routes";
 import { AuthProvider } from "./context/AuthContext";
 function App() {
   return (
-   <AuthProvider>
-     <Router>
-      <Routes>
-      {
-        routes.map((ele)=>{
-          return <Route path={ele.path} key={ele.path} element={ele.element}></Route>
-        })
-      }
-      </Routes>
-    </Router>
-   </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {routes.map((ele) => {
+            return (
+              <Route
+                path={ele.path}
+                key={ele.path}
+                element={ele.element}
+              ></Route>
+            );
+          })}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
- 
+
 export default App;
