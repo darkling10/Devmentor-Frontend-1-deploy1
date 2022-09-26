a84e43b1f8ffd7d7fe05759aa1df185b03b6cff4
vinit <khollam.vinit@gmail.com>
2022-09-26 20:23:22 +0530

    filter ui

--- src/pages/InterviewExp.jsx
+++ src/pages/InterviewExp.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import InterviewFilter from '../components/InterviewFilter'
 
 function InterviewExp() {
   return (
-    <div className='w-full'>
+    <div className='w-full bg-blue-50/70 min-h-screen '>
 
-    <div className="w-full py-10 min-h-[40vh] bg-int"/>
+    <div className="w-full py-10 min-h-[40vh] bg-int">
+        <div className="flex items-center justify-center w-full min-h-[20vh]">
+        <h1 className='w-full text-center font-semibold text-4xl text-white'>Interview Experiences</h1>
+        </div>
+    </div>
 
-    
+    <InterviewFilter />
 
     </div>
   )
